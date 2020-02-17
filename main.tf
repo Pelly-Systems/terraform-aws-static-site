5b21962e815fb3aab772a63b18aa00ec49c311e8
Mark Woodhall <mark.woodhall@gmail.com>
2020-02-17 12:00:06 +0000

    Remove interpolation only expression

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_s3_bucket" "main" {
 }
 
 resource "aws_s3_bucket_public_access_block" "main_acl_policy" {
-  bucket = "${aws_s3_bucket.main.id}"
+  bucket = aws_s3_bucket.main.id
 
   block_public_acls   = true
   block_public_policy = true
